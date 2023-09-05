5eac491de067b102844a6668fad091d2395dd41e
dongeon110 <dongeon110@gmail.com>
2023-09-05 17:42:33 +0900

    :heavy_plus_sign: Add SecurityConfig

---
+++ src/main/java/com/note/security/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.note.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration // 스프링의 환경설정 파일을 의미
+@EnableWebSecurity // 모든 요청 URL이 시큐리티의 제어를 받도록 하는 애너테이션
+public class SecurityConfig {
+    @Bean // 세부설정은 SecurityFilterChain 빈을 생성하여 설정
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // 인증되지 않은 요청을 허락한다는 의미 (로그인 필요 X)
+        http.authorizeHttpRequests(
+                (authorizeHttpRequests) -> authorizeHttpRequests
+                        .requestMatchers(new AntPathRequestMatcher("/**")).permitAll());
+        return http.build();
+    }
+}
