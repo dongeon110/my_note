c3067b0a5a0e24cd1a2245bbe11ea6ab27a4048d
dongeon110 <dongeon110@gmail.com>
2023-09-08 13:32:01 +0900

    :sparkles: Initial springdoc-openapi /swagger-ui/index.html

--- src/main/java/com/note/board/web/BoardRestController.java
+++ src/main/java/com/note/board/web/BoardRestController.java
@@ -4,9 +4,7 @@ import com.note.utils.ResponseDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -18,6 +16,29 @@ public class BoardRestController {
         ResponseDTO responseDTO = ResponseDTO.builder()
                 .message("This is Message.")
                 .build();
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(responseDTO);
+    }
+    @GetMapping("/asdf.do")
+    public ResponseEntity asdf(
+            @RequestParam(value="hi", required = false, defaultValue = "") String hi,
+            @RequestParam(value="hello") String hello
+    ) {
+        log.info("hi: {}", hi);
+        log.info("hello: {}", hello);
+        if(hi.equals("no")){
+            ResponseDTO responseDTO = ResponseDTO.builder()
+                    .message("Failed.")
+                    .build();
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(responseDTO);
+        }
+
+        ResponseDTO responseDTO = ResponseDTO.builder()
+                .message("Success.")
+                .build();
+
         return ResponseEntity.status(HttpStatus.OK)
                 .body(responseDTO);
     }
