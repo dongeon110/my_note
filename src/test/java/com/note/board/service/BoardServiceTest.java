6e9827b77c37aa269661f2002ad4b74a88476329
dongeon110 <dongeon110@gmail.com>
2023-11-23 13:56:14 +0900

    :white_check_mark: Test: findById, delete

--- src/test/java/com/note/board/service/BoardServiceTest.java
+++ src/test/java/com/note/board/service/BoardServiceTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -23,16 +24,37 @@ class BoardServiceTest {
     @Autowired
     private BoardRepository boardRepository;
 
+    private Long testBoardIndex = 3L;
+
     @Test
     @DisplayName("전체 조회시 결과 있음")
     void getBoardList_When_전체조회_Expect_결과있음() {
         //given
         //when
-        List<Board> boardList = boardService.getBoardList();
+        List<Board> boardList = boardService
+                .getBoardList();
         //then
         assertThat(boardList)
                 .isNotEmpty();
+    }
 
+    @Test
+    @DisplayName("테스트 Board 데이터 1개 조회")
+    void getBoard_Expect_테스트_제목() {
+        //given
+        Board expectedBoard = new Board();
+        expectedBoard.setBoardIndex(testBoardIndex);
+        expectedBoard.setTitle("테스트 제목");
+        expectedBoard.setWriter(1L);
+
+        //when
+        Board actualBoard = boardService
+                .getBoard(testBoardIndex)
+                .get();
+
+        //then
+        assertThat(actualBoard)
+                .isEqualTo(expectedBoard);
     }
 
     @Test
@@ -44,7 +66,8 @@ class BoardServiceTest {
         board.setTitle("테스트 제목");
         board.setWriter(1L); // Test
         //when
-        Board resultBoard = boardService.saveBoard(board);
+        Board resultBoard = boardService
+                .saveBoard(board);
         //then
         //id
         assertThat(resultBoard.getBoardIndex())
@@ -53,6 +76,21 @@ class BoardServiceTest {
 
     @Test
     @DisplayName("삭제 테스트")
+    @Transactional
     void delete() {
+        //given
+        Board board = new Board();
+        board.setBoardIndex(testBoardIndex); // 삭제할 데이터
+        
+        getBoard_Expect_테스트_제목(); // 기존 테스트 데이터는 있어야 함
+
+        //when
+        boardService.delete(board);
+
+        //then
+        Optional<Board> maybeBoard = boardService.getBoard(testBoardIndex);
+
+        assertThat(maybeBoard)
+                .isEmpty();
     }
 }
